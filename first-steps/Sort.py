70c33e5abff6f419573f9f55d95c263ddd528da6
Serge Farny <serge.farny@gmail.com>
2017-11-09 21:52:56 +0100

    Add quicksort

--- first-steps/Sort.py
+++ first-steps/Sort.py
@@ -29,20 +29,58 @@ def mergesort(t, s=0, e=None):
     return r
 
 
+def quicksort(A, lo=0, hi=None):
+    if hi is None:
+        hi = len(A) - 1
+
+    if lo < hi:
+        p = partition(A, lo, hi)
+        quicksort(A, lo, p)
+        quicksort(A, p + 1, hi)
+
+
+def partition(A, lo, hi):
+    pivot = A[lo]
+    i = lo - 1
+    j = hi + 1
+
+    while True:
+        i += 1
+        j -= 1
+
+        while A[i] < pivot:
+            i += 1
+
+        while A[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        tmp = A[i]
+        A[i] = A[j]
+        A[j] = tmp
+
 if __name__ == '__main__':
     X = [1, 4, 9, 1, 2, 0, 7, 3, 7, 9, 10, 23, 93, 1, 32, 4, 46]
     Y = [4, 5, 3, 2, 3, 5, 4]
 
     pSorted = sorted(X)
     mSorted = mergesort(X)
+    qSorted = X[:]
+    quicksort(qSorted)
 
     print(X)
     print(pSorted)
     print(mSorted)
+    print(qSorted)
 
     pSorted = sorted(Y)
     mSorted = mergesort(Y)
+    qSorted = Y[:]
+    quicksort(qSorted)
 
     print(Y)
     print(pSorted)
     print(mSorted)
+    print(qSorted)
