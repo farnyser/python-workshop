c1d5d198d5e884347067480f6f144348679be659
Serge Farny <serge.farny@gmail.com>
2017-11-12 15:41:21 +0100

    Weighted Reservoir Sampling

--- first-steps/Sampling.py
+++ first-steps/Sampling.py
@@ -24,6 +24,30 @@ class ReservoirSample:
         return self.samples[item]
 
 
+class WeightedReservoirSample:
+    def __init__(self, size):
+        self.maximum_size = size
+        self.weight = 0
+        self.samples = []
+
+    def __len__(self):
+        return len(self.samples)
+
+    def append(self, x, w):
+        self.weight += w / (1+len(self))
+        if len(self) < self.maximum_size:
+            self.samples.append(x)
+            return
+
+        probability = w / self.weight
+        t = random.random()
+        if t < probability:
+            self.samples[random.randint(0,self.maximum_size-1)] = x
+
+    def __getitem__(self, item):
+        return self.samples[item]
+
+
 if __name__ == '__main__':
     S = ReservoirSample(10)
 
@@ -32,4 +56,16 @@ if __name__ == '__main__':
         S.append(input)
 
     for x in S:
+        print(x)
+
+
+    print("---")
+
+    WS = WeightedReservoirSample(10)
+
+    for x in range(1000000):
+        input = random.randint(0,1000)
+        WS.append(input, 100*(input % 2) + 1)
+
+    for x in WS:
         print(x)
